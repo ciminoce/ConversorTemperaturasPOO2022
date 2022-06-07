3a4c735722d64bdc78bac84e5fd3edfad0b9d5eb
ciminoce <ciminoce@yahoo.com.ar>
2022-06-07 10:23:19 -0300

    Agregar archivos de proyecto.

---
+++ ConversorTemperaturasPOO2022.Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConversorTemperaturasPOO2022.Entidades;
+
+namespace ConversorTemperaturasPOO2022.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = 20;
+            Console.WriteLine($"{celsius} grados equivalen a {Conversor.ConvertirDeCelsiusAFahrenheit(celsius)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConversorTemperaturasPOO2022.Entidades/Conversor.cs
@@ -0,0 +1,36 @@
+﻿namespace ConversorTemperaturasPOO2022.Entidades
+{
+    public static class Conversor
+    {
+        private static float CeroAbsoluto = 273.15f;
+        public static float ConvertirDeCelsiusAKelvin(float celsius)
+        {
+            return celsius + CeroAbsoluto;
+        }
+
+        public static float ConvertirDeKelvinACelsius(float kelvin)
+        {
+            return kelvin - CeroAbsoluto;
+        }
+
+        public static float ConvertirDeCelsiusAFahrenheit(float celsius)
+        {
+            return 1.8f * celsius + 32;
+        }
+
+        public static float ConvertirDeFahrenheitACelsius(float fah)
+        {
+            return (fah - 32) / 1.8f;
+        }
+
+        public static float ConvertirDeCelsiusARankine(float celsius)
+        {
+            return 1.8f * ConvertirDeCelsiusAKelvin(celsius);
+        }
+
+        public static float ConvertirDeRankineACelsius(float rankine)
+        {
+            return (rankine / 1.8f) - CeroAbsoluto;
+        }
+    }
+}
